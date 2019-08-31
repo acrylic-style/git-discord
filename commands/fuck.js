3afdba90dd79e6b3065aeb3f90104d0086e71947
acrylic-style <me@acrylicstyle.xyz>
2019-09-01 07:17:03 +0900

    ewwww

--- commands/fuck.js
+++ commands/fuck.js
@@ -6,6 +6,13 @@ module.exports = class extends Command {
   }
 
   async run(msg) {
-    msg.channel.send({ files: [ 'https://img.acrylicstyle.xyz/upload/162158118190/736584104/fuck.PNG' ] })
+    const images = [
+      'https://img.acrylicstyle.xyz/upload/162158118190/736584104/fuck.PNG',
+      'https://img.acrylicstyle.xyz/upload/16215811858/58580369/fuck2.PNG',
+      'https://img.acrylicstyle.xyz/upload/16215811858/8810387010/fuck3.PNG',
+      'https://img.acrylicstyle.xyz/upload/16215811858/511092976/fuck4.PNG',
+      'https://img.acrylicstyle.xyz/upload/16215811858/210104588/fuck5.PNG',
+    ]
+    msg.channel.send({ files: [ images[Math.floor(Math.random()*images.length)] ] })
   }
 }
