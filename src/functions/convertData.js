3e4439673f407a1d64a0c15dd5dbfa360fb184d5
acrylic-style <me@acrylicstyle.xyz>
2020-11-12 03:17:27 +0900

    try using unused value

--- src/functions/convertData.js
+++ src/functions/convertData.js
@@ -19,23 +19,23 @@ const Discord = require('discord.js') // eslint-disable-line
  */
 module.exports = async (type, data, date) => {
   const { client, lang } = require('../../client')
-  //const channel = await client.channels.fetch(data.split(',')[0]) || data.split(',')[1] // eslint-disable-line
+  const channel = await client.channels.fetch(data.split(',')[0]) || data.split(',')[1]
   const ds = new Date(date).toLocaleString()
   switch (type) {
     case 'channelCreate':
     case 'channelDelete':
     case 'emojiCreate':
     case 'emojiDelete':
-      return { data: await client.channels.fetch(data), description: f(lang.convert[type], data.split(',')[1], ds) }
+      return { data: channel, description: f(lang.convert[type], data.split(',')[1], ds) }
     case 'channelNameUpdate':
-      return { data: await client.channels.fetch(data), description: f(lang.convert[type], data.split(',')[1], data.split(',')[2], ds) }
+      return { data: channel, description: f(lang.convert[type], data.split(',')[1], data.split(',')[2], ds) }
     case 'channelParentUpdate':
-      return { data: await client.channels.fetch(data), description: f(lang.convert[type], data.split(',')[1], data.split(',')[5].replace(/<colon>/g, ','), data.split(',')[4].replace(/<colon>/g, ','), ds) }
+      return { data: channel, description: f(lang.convert[type], data.split(',')[1], data.split(',')[5].replace(/<colon>/g, ','), data.split(',')[4].replace(/<colon>/g, ','), ds) }
     case 'channelTopicUpdate':
-      return { data: await client.channels.fetch(data), description: f(lang.convert[type], data.split(',')[1], ds) }
+      return { data: channel, description: f(lang.convert[type], data.split(',')[1], ds) }
     case 'channelPositionUpdate':
     case 'channelNSFWUpdate':
-      return { data: await client.channels.fetch(data), description: f(lang.convert[type], data.split(',')[1], data.split(',')[3], data.split(',')[2], ds) }
+      return { data: channel, description: f(lang.convert[type], data.split(',')[1], data.split(',')[3], data.split(',')[2], ds) }
     default:
       return { data, description: f(lang.convert.no_description, data, ds) }
   }
